c427355acc0f67b6b84763c42115c19c71902434
sevichok <sevichok41294@gmail.com>
2022-01-19 21:36:52 +0300

    some logic with useTheme in Todos, Users

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import Button from '../Button/Button';
 
 const TodoWrapper = styled('div')`
   flex: 1 0 calc(25% - 8px);
   display: flex;
   flex-direction: column;
-  border: 1.5px solid gray;
   border-radius: 6px;
   box-sizing: border-box;
   padding: 8px;
   margin: 4px;
-  background: white;
+  background-color: ${(props) => props.theme.backgroundColor.item};
+  color: ${(props) => props.theme.color.first};
 
   > h6 {
     font-size: 16px;
     font-weight: 600;
-    border-bottom: 1px solid gray;
+    border-bottom: 1px solid ${(props) => props.theme.button.backgroundColor};
     padding-bottom: 8px;
     margin-top: 0px;
     margin-bottom: 8px;
@@ -29,11 +30,7 @@ const TodoWrapper = styled('div')`
   }
 
   > button {
-    color:white;
-    background: hsl(144,100%,19%);
     width: 100%;
-    margin-top: auto;
-    border-radius: 6px;
   }
 `;
 
@@ -49,7 +46,7 @@ function Todo({ id, title, completed }) {
       <h6>Title: {title}</h6>
       <p>Id: {id}</p>
       <p>Completed: {completed.toString()}</p>
-      <button onClick={handleClick}>Detail</button>
+      <Button onClick={handleClick}>Detail</Button>
     </TodoWrapper>
   )
 }
